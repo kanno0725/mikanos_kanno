ea494711599f8707b00998689e2c5f89124d0b1f
kanno <kanno.ooroor@gmail.com>
2025-06-04 01:01:08 +0900

    add osbook_day09e

--- kernel/console.hpp
+++ kernel/console.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <memory>
 #include "graphics.hpp"
+#include "window.hpp"
 
 class Console {
   public:
@@ -9,12 +11,14 @@ class Console {
     Console(const PixelColor& fg_color, const PixelColor& bg_color);
     void PutString(const char* s);
     void SetWriter(PixelWriter* writer);
+    void SetWindow(const std::shared_ptr<Window>& window);
 
   private:
     void Newline();
     void Refresh();
 
     PixelWriter* writer_;
+    std::shared_ptr<Window> window_;
     const PixelColor fg_color_, bg_color_;
     // 行末にヌル文字('\0')を書くためkColumnsに1足す
     char buffer_[kRows][kColumns + 1];
