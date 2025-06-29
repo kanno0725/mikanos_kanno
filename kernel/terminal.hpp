c37adaddc6b0f7c6ba82e937aa155a9ea61b09ee
kanno <kanno.ooroor@gmail.com>
2025-06-30 00:26:45 +0900

    add 29d ターミナルの再描画処理を修正

--- kernel/terminal.hpp
+++ kernel/terminal.hpp
@@ -44,6 +44,7 @@ struct TerminalDescriptor {
 
     Task& UnderlyingTask() const { return task_; }
     int LastExitCode() const { return last_exit_code_; }
+    void Redraw();
 
   private:
    std::shared_ptr<ToplevelWindow> window_;
